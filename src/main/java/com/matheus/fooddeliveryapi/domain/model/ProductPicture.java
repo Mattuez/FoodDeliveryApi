97c1d2d9728ad793f8fafbebf3b493238387aeb6
matheuslins45@gmail.com <matheuslins45@gmail.com>
2024-07-19 17:11:59 -0300

    Abstract Class Picture created, ready to be extend by other classes

--- src/main/java/com/matheus/fooddeliveryapi/domain/model/ProductPicture.java
+++ src/main/java/com/matheus/fooddeliveryapi/domain/model/ProductPicture.java
@@ -6,29 +6,12 @@ import lombok.EqualsAndHashCode;
 import javax.persistence.*;
 
 @Data
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
 @Entity
 @Table(
         name = "product_picture"
 )
-public class ProductPicture {
-
-    @Id
-    @EqualsAndHashCode.Include
-    @Column(name = "product_id")
-    private Long id;
-
-    @Column(name = "file_name")
-    private String fileName;
-
-    @Column(name = "description")
-    private String description;
-
-    @Column(name = "content_type")
-    private String contentType;
-
-    @Column(name = "size")
-    private Long size;
+public class ProductPicture extends Picture {
 
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId
